273103d194c057f2eb91e2717ad0e8e67945175f
Mustafa Magdy <45540037+Mustafa-Magdyy@users.noreply.github.com>
2019-05-01 19:11:31 +0200

    Update QuestionRepository.java

--- QuestionRepository.java
+++ QuestionRepository.java
@@ -1,9 +1,15 @@
-package spring.quiz.question;
+package com.question;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
 
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface QuestionRepository extends CrudRepository<Question, Integer>{
-	public List<Question> findAllByQuizTitle(String title);
+	public List<Question>findAllByQuizTitle(String title);
+	public Question findByStatement(String statement);
+    
 }
