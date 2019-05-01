ecc258a7ce8447362ece925dc43ddacd8e3abae3
Mustafa Magdy <45540037+Mustafa-Magdyy@users.noreply.github.com>
2019-05-01 19:14:55 +0200

    Update QuizController.java

--- QuizController.java
+++ QuizController.java
@@ -1,42 +1,51 @@
-package spring.quiz.quiz;
+package com.quiz;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
-@RestController
+@Controller
+@RequestMapping("/quizzes")
 public class QuizController {
-
-	@Autowired
-	private QuizService quizService;
-
-	@RequestMapping("/quizzes")
-	public ModelAndView getAllQuizes(Model model) {
-		ModelAndView modelAndView = new ModelAndView();
-	    modelAndView.setViewName("getAllQuizzes");
-	    model.addAttribute("quizzes", quizService.getAllQuizes());
-	    
-	    return modelAndView;
-	}
-
-	@RequestMapping(method = RequestMethod.POST, value = "/quizzes")
-	public void addQuiz(@RequestBody Quiz quiz) {
-		quizService.addQuiz(quiz);
+	private final QuizService quizService;
+	
+	public QuizController(QuizService quizService) {
+		this.quizService = quizService;
 	}
 	
-	@RequestMapping("/quizzes/title")
-	public Quiz getAllByTitle(@RequestParam("name") String name) {
-		return quizService.getByTitle(name);
+	@GetMapping
+	public String getAllQuizzes(Model model) {
+		List<Quiz> quizzes = quizService.getAllQuizzes();
+		model.addAttribute("quizzes", quizzes);
+		return "quizzes";
 	}
-
-	@RequestMapping("/quizzes/skill")
-	public List<Quiz> getAllBySkill(@RequestParam("name") String name) {
-		return quizService.getBySkill(name);
+	@RequestMapping(value = "/{skill}")
+	public String getAllBySkill(Model model,@PathVariable String skill) {
+		List<Quiz> quizzes = quizService.getAllBySkill(skill);
+		model.addAttribute("quizzes", quizzes);
+		return "quizzes";
+	}
+	@RequestMapping(value = "/addQuiz")
+	public String addQuiz() {
+		return "addQuiz";
+	}
+	
+	@RequestMapping(value = "/{quizTitle}/counter")
+	public RedirectView updateCounter(@PathVariable(name = "quizTitle") String title) {
+		quizService.increment(title);
+		return new RedirectView("/quizzes");
+	}
+	
+	@PostMapping
+	public RedirectView addQuiz(Model model, @RequestParam(name = "title") String title, @RequestParam(name = "skill") String skill, @RequestParam(name = "passScore") Integer passScore, @RequestParam(name = "duration") Integer duration) {
+		quizService.addQuiz(new Quiz(title, skill, passScore, duration));
+		return new RedirectView("/quizzes");
 	}
 }
